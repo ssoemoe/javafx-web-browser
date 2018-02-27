09d10d13c1b1ae357309e36dee873e315ea45f1d
Shane Moe <ssoemoe@terpmail.umd.edu>
2018-02-26 19:18:22 -0500

    Add refresh button
    Add refresh button for refeshing webpages

--- src/view/MainBrowser.java
+++ src/view/MainBrowser.java
@@ -14,9 +14,12 @@ import javafx.concurrent.Worker.State;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
@@ -28,6 +31,8 @@ public class MainBrowser extends Application {
 	private final WebEngine ENGINE = BROWSER.getEngine();
 	private TextField urlField = new TextField();
 	private final ScrollPane SCROLL_PANE = new ScrollPane();
+	private Button refreshButton;
+	private String urlString;
 
 	/*main method*/
 	public static void main(String[] args) {
@@ -36,7 +41,13 @@ public class MainBrowser extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-
+		
+		/*Main Vertical View*/
+		VBox mainView = new VBox(20);
+		
+		//Scene for window
+		Scene scene = new Scene(mainView);
+		
 		//url field setting
 		urlField.setPromptText("Enter URL here");
 
@@ -54,11 +65,12 @@ public class MainBrowser extends Application {
 				String urlHttp = urlChars[0] + urlChars[1] + urlChars[2] + urlChars[3] + "";
 
 				if(!urlHttp.equalsIgnoreCase("http")) {
-					ENGINE.load("http://" + urlField.getText());
+					urlString = "http://" + urlField.getText(); 
+					ENGINE.load(urlString);
 				}
 
 				else {
-					ENGINE.load(urlField.getText());
+					ENGINE.load(urlString);
 				}
 			}
 		});
@@ -73,14 +85,22 @@ public class MainBrowser extends Application {
 			}	
 		});
 
-		/*Main Vertical View*/
-		VBox mainView = new VBox(20);
-		mainView.getChildren().addAll(urlField, SCROLL_PANE);
+		/*Refresh the web page*/
+		refreshButton = new Button("Refresh");
+		refreshButton.setOnAction(e -> {
+			ENGINE.load(urlString);
+		});
+		
+		/*Top Panel*/
+		HBox topPanel = new HBox(10);
+		topPanel.getChildren().addAll(refreshButton, urlField);
+		topPanel.setHgrow(urlField, Priority.ALWAYS);
+		
+		/*Set the main vertical view*/
+		mainView.getChildren().addAll(topPanel, SCROLL_PANE);
 		for(Node child : mainView.getChildren()) {
 			mainView.setMargin(child, new Insets(10, 10, 10, 10));
 		}
-
-		Scene scene = new Scene(mainView);
 
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Shane's Browser");
